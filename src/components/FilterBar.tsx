1fdb2591bd2a8422422eef61b384c1c4c5474d09
minhhuylqd <minhhuy2810@gmail.com>
2024-10-08 18:18:17 +0300

    Implement FilterBar clear text button

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '@/styles/FilterBar.module.scss';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 type FilterBarProps = {
   onFilterChange: (filter: string) => void;
 };
 
 const FilterBar: React.FC<FilterBarProps> = ({ onFilterChange }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    onFilterChange(e.target.value);
+  };
+
+  const clearInput = () => {
+    setInputValue('');
+    onFilterChange('');
+  };
+
   return (
-    <input
-      type="text"
-      placeholder="Search users by name..."
-      onChange={(e) => onFilterChange(e.target.value)}
-      className={styles.filterBar}
-    />
+    <div className={styles.filterBarContainer}>
+      <input
+        type="text"
+        placeholder="Search users by name..."
+        value={inputValue}
+        onChange={handleInputChange}
+        className={styles.filterBar}
+      />
+      {inputValue && (
+        <button className={styles.clearButton} onClick={clearInput}>
+          <XMarkIcon className={styles.xmarkIcon} />
+        </button>
+      )}
+    </div>
   );
 };
 
